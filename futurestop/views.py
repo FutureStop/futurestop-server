e066b388fbc12fd2db1007d8d7a60cca3e792dae
Ethan Sherman <ethan.sherman@mutualmobile.com>
2013-03-09 20:50:18 -0600

    updated settings and views

--- futurestop/views.py
+++ futurestop/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpResponse
+import json
 
-from models import Vehicle, Person, Election
+from django.http import HttpResponse, HttpResponseBadRequest
+
+from models import Person, Election
 
 
 def udid(request, udid=None):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+        except KeyError:
+            return HttpResponseBadRequest('missing udid')
+        created, person = Person.objects.get_or_create(udid=udid)
     return HttpResponse('udid {0}'.format(udid))
+
+
+def election(request):
+    pass
