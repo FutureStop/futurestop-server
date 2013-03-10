17918e14f45028b4bc45c8fc5ba261cd46625656
Jake Basile <me@jakebasile.com>
2013-03-09 19:39:24 -0600

    added db configuration.

--- futurestop/settings.py
+++ futurestop/settings.py
@@ -1,4 +1,5 @@
 # Django settings for futurestop project.
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,15 +11,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/tmp/futurestop.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config(default='sqlite:////var/tmp/futurestop.db'),
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
